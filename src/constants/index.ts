f23b10f9bac48543b53b48c2dc0afea1d3e4d10b
hankliu <397694072@qq.com>
2024-05-07 08:49:41 +0800

    feat(parse-bilibili): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const PageTitle = 'H.L Toolkits - 解析 bilibili 视频';
+
+export const PageSubTitle = `在艺术的海洋里，Bilibili是一座闪耀的明灯，而我们的网站则是那道璀璨的星光，为您打开通往B站视频的奇妙之门。无需等待，无需插件，只需轻轻一点，即可畅游于数以亿计的精彩视频之中。让我们携手探索Bilibili的无限魅力，与创意、激情、与感动同行，让每一帧画面都成为生活中的艺术杰作。`;
